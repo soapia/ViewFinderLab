96ae1b9667975b9324912fa5c3ed90feb96459d2
soapia <sofia.ongele@ealas.org>
2019-05-23 15:21:17 -0400

    THANK YOU JESUS she done

--- ViewFinder/PhotoTableViewController.swift
+++ ViewFinder/PhotoTableViewController.swift
@@ -78,25 +78,30 @@ class PhotoTableViewController: UITableViewController {
     }
 
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+                let photoToDelete = photos[indexPath.row]
+                context.delete(photoToDelete)
+                (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+                getPhotos()
+                //save to context, then reload
+            }
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
